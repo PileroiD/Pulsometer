38c3bf8c3ab48d3dc5fd1507f4d5a2464ce6baff
Alex <pileroid@gmail.com>
2021-12-26 17:08:34 +0200

    Added adaptation for project

--- src/js/script.js
+++ src/js/script.js
@@ -11,24 +11,18 @@ $(document).ready(function(){
         </button>`,
         responsive: [
         {
-          breakpoint: 768,
-          settings: {
-            dots: true,
-            arrows: false,
-          }
+            breakpoint: 992,
+            settings: {
+                dots: true,
+                arrows: false,
+            }
+
         },
         {
-          breakpoint: 600,
-          settings: {
-            slidesToShow: 2,
-            slidesToScroll: 2
-          }
-        },
-        {
-          breakpoint: 480,
-          settings: {
-            slidesToShow: 1,
-            slidesToScroll: 1
+            breakpoint: 768,
+            settings: {
+                dots: true,
+                arrows: false,
           }
         }
         ]
